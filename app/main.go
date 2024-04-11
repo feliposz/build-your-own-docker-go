b1b5b7c16e0d467784050becb993836d2a2381db
feliposz <feliposz@gmail.com>
2024-04-11 14:41:13 -0300

    Downloading and parsing config

--- app/main.go
+++ app/main.go
@@ -95,11 +95,15 @@ func copyFile(src, dest string) error {
 	return err
 }
 
+// https://distribution.github.io/distribution#token-response-fields
+
 type Auth struct {
 	Token     string `json:"token"`
 	ExpiresIn int    `json:"expires_in"`
 	IssuedAt  string `json:"issued_at"`
 }
+
+// https://distribution.github.io/distribution#image-manifest
 
 type Manifest struct {
 	SchemaVersion int              `json:"schemaVersion"`
@@ -114,6 +118,51 @@ type ManifestConfig struct {
 	Digest    string `json:"digest"`
 }
 
+// https://github.com/opencontainers/image-spec/blob/main/schema/config-schema.json
+
+type Config struct {
+	Architecture    string          `json:"architecture"`
+	Config          ContainerConfig `json:"config"`
+	Container       string          `json:"container"`
+	ContainerConfig ContainerConfig `json:"container_config"`
+	Created         string          `json:"created"`
+	Docker_version  string          `json:"docker_version"`
+	History         []ConfigHistory `json:"history"`
+	Os              string          `json:"os"`
+	Rootfs          ConfigRootFS    `json:"rootfs"`
+}
+
+type ContainerConfig struct {
+	Hostname     string
+	Domainname   string
+	User         string
+	AttachStdin  bool
+	AttachStdout bool
+	AttachStderr bool
+	Tty          bool
+	OpenStdin    bool
+	StdinOnce    bool
+	Env          []string
+	Cmd          []string
+	Image        string
+	Volumes      interface{}
+	WorkingDir   string
+	Entrypoint   interface{}
+	OnBuild      interface{}
+	Labels       interface{}
+}
+
+type ConfigHistory struct {
+	Created    string `json:"created"`
+	CreatedBy  string `json:"created_by"`
+	EmptyLayer bool   `json:"empty_layer"`
+}
+
+type ConfigRootFS struct {
+	Type    string   `json:"type"`
+	DiffIDs []string `json:"diff_ids"`
+}
+
 func getDockerImage(image string) error {
 	parts := strings.Split(image, ":")
 	if len(parts) == 1 {
@@ -121,6 +170,9 @@ func getDockerImage(image string) error {
 	}
 
 	log.Println("get an auth token")
+
+	// https://distribution.github.io/distribution#getting-a-bearer-token
+	// https://distribution.github.io/distribution#using-the-signed-token
 
 	authResp, err := http.Get(fmt.Sprintf("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/%s:pull", parts[0]))
 	if err != nil {
@@ -134,6 +186,8 @@ func getDockerImage(image string) error {
 	}
 	var auth Auth
 	json.Unmarshal(authData, &auth)
+
+	// https://distribution.github.io/distribution/#pulling-an-image-manifest
 
 	log.Println("get the manifest for ", image)
 
@@ -161,6 +215,26 @@ func getDockerImage(image string) error {
 		}
 	}
 
+	// https://distribution.github.io/distribution#pulling-a-layer
+
+	log.Println("downloading config ", manifest.Config.Digest)
+	configReq, err := http.NewRequest("GET", fmt.Sprintf("https://registry.hub.docker.com/v2/library/%s/blobs/%s", parts[0], manifest.Config.Digest), nil)
+	configReq.Header.Add("Authorization", fmt.Sprintf("Bearer %s", auth.Token))
+	configReq.Header.Add("Accept", manifest.Config.MediaType)
+	configResp, err := http.DefaultClient.Do(configReq)
+	if err != nil {
+		return err
+	}
+	defer configResp.Body.Close()
+	configData, err := io.ReadAll(configResp.Body)
+	if err != nil {
+		return err
+	}
+	var config Config
+	json.Unmarshal(configData, &config)
+
+	fmt.Printf("%#v\n", config)
+
 	for _, layer := range manifest.Layers {
 		layerPath := filepath.Join(imagesDir, strings.Replace(layer.Digest, ":", "_", 1))
 
@@ -179,6 +253,8 @@ func getDockerImage(image string) error {
 			return err
 		}
 		defer file.Close()
+
+		// https://distribution.github.io/distribution#pulling-a-layer
 
 		log.Println("downloading layer ", layer.Digest)
 		layerReq, err := http.NewRequest("GET", fmt.Sprintf("https://registry.hub.docker.com/v2/library/%s/blobs/%s", parts[0], layer.Digest), nil)
--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 set -e
 COURSE_DEFINITION=/mnt/v/GitHub/_others/docker-tester/internal/test_helpers/course_definition.yml
 export CODECRAFTERS_SUBMISSION_DIR=/mnt/v/GitHub/feliposz/build-your-own-docker-go
-export CODECRAFTERS_TEST_CASES_JSON=$(yq -I0 '[.stages[] | {"slug": .slug, "tester_log_prefix": .slug, "title": (path | .[-1] + 1) + ") " + .name }]' -o=json $COURSE_DEFINITION)
+export CODECRAFTERS_TEST_CASES_JSON=$(yq -I0 '[.stages[5] | {"slug": .slug, "tester_log_prefix": .slug, "title": (path | .[-1] + 1) + ") " + .name }]' -o=json $COURSE_DEFINITION)
 go build -buildvcs="false" -o mydocker ./app/main.go
 /mnt/v/GitHub/_others/docker-tester/docker-tester
