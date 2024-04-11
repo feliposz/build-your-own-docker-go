ace221fcacc1f0ccd01c36075a6ed40c6b6a14aa
feliposz <feliposz@gmail.com>
2024-04-11 02:46:40 -0300

    Fetching manifest from the Docker Registry

--- app/main.go
+++ app/main.go
@@ -1,11 +1,18 @@
+//go:build linux
+// +build linux
+
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"syscall"
 )
 
@@ -19,8 +26,11 @@ func main() {
 	log.SetOutput(logger)
 	defer logger.Close()
 
+	image := os.Args[2]
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+
+	getDockerImage(image)
 
 	log.Println("making temp dir")
 	tempDir, err := os.MkdirTemp(os.TempDir(), "mydocker")
@@ -86,4 +96,66 @@ func copyFile(src, dest string) error {
 	defer dstFile.Close()
 	_, err = io.Copy(dstFile, srcFile)
 	return err
+}
+
+type Auth struct {
+	Token     string `json:"token"`
+	ExpiresIn int    `json:"expires_in"`
+	IssuedAt  string `json:"issued_at"`
+}
+
+type Manifest struct {
+	SchemaVersion int              `json:"schemaVersion"`
+	MediaType     string           `json:"mediaType"`
+	Config        ManifestConfig   `json:"config"`
+	Layers        []ManifestConfig `json:"layers"`
+}
+
+type ManifestConfig struct {
+	MediaType string `json:"mediaType"`
+	Size      int    `json:"size"`
+	Digest    string `json:"digest"`
+}
+
+func getDockerImage(image string) error {
+	parts := strings.Split(image, ":")
+	if len(parts) == 1 {
+		parts = append(parts, "latest")
+	}
+
+	log.Println("get an auth token")
+
+	authResp, err := http.Get(fmt.Sprintf("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/%s:pull", parts[0]))
+	if err != nil {
+		return err
+	}
+	defer authResp.Body.Close()
+
+	authData, err := io.ReadAll(authResp.Body)
+	if err != nil {
+		return err
+	}
+	var auth Auth
+	json.Unmarshal(authData, &auth)
+
+	log.Println("get the manifest for ", image)
+
+	manifestReq, err := http.NewRequest("GET", fmt.Sprintf("https://registry.hub.docker.com/v2/library/%s/manifests/%s", parts[0], parts[1]), nil)
+	manifestReq.Header.Add("Authorization", fmt.Sprintf("Bearer %s", auth.Token))
+	manifestReq.Header.Add("Accept", "application/vnd.docker.distribution.manifest.v2+json")
+	manifestResp, err := http.DefaultClient.Do(manifestReq)
+	if err != nil {
+		return err
+	}
+	defer manifestResp.Body.Close()
+	manifestData, err := io.ReadAll(manifestResp.Body)
+	if err != nil {
+		return err
+	}
+	var manifest Manifest
+	json.Unmarshal(manifestData, &manifest)
+
+	fmt.Println(manifest)
+
+	return nil
 }
